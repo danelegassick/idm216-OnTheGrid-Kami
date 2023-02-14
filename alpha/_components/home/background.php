bfa30103b811e166b0b748e6d0404ace5aeaa81e
avani <ay434@drexel.edu>
2023-02-13 21:40:56 -0500

    stashed changes restored

--- alpha/_components/home/background.php
+++
@@ -1,9 +0,0 @@
-<div class="background-swirls">
-    <img src=<?php echo '"' . site_url() . '/dist/images/swirl.png"' ?> alt="swirl" class="swirl">
-    <img src=<?php echo '"' . site_url() . '/dist/images/swirl.png"' ?> alt="swirl" class="swirl">
-    <img src=<?php echo '"' . site_url() . '/dist/images/swirl.png"' ?> alt="swirl" class="swirl">
-    <img src=<?php echo '"' . site_url() . '/dist/images/swirl.png"' ?> alt="swirl" class="swirl">
-    <img src=<?php echo '"' . site_url() . '/dist/images/swirl.png"' ?> alt="swirl" class="swirl">
-</div>
-
-<img class="kami-logo" src=<?php echo '"' . site_url() . '/dist/images/kami-logo.svg"' ?> alt="kami-logo">
--- alpha/_components/home/intro.php
+++
@@ -1,7 +0,0 @@
-<div class="intro">
-    <p class="welcome">Welcome to</p>
-    <p class="welcome-2">Kami Food Truck!</p>
-    <p class="welcome-3">Open Weekdays 11am - 5:30pm</p>
-    <br>
-    <a href="#" class="btn-1">Begin Order</a>
-</div>
