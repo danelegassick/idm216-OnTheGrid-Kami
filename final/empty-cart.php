6c06d557d88b4ea6fbe80e06af409fafe480d755
leelaur <laurenlee552@yahoo.com>
2023-03-10 00:11:50 -0500

    margin/button update, edit/delete icon

--- final/empty-cart.php
+++ final/empty-cart.php
@@ -13,7 +13,7 @@ $drinks = get_drinks();
 <div class="orders-not-signed-in">
     <img src="<?php echo site_url();?>/dist/images/kami-logo.svg" alt="" class="greyscale-cook">
     <h1 class="sign-up-cta">Your cart is empty</h1>
-    <a href="<?php echo site_url(); ?>/index.php" class="button btn-dark">Start Order</a>
+    <a href="<?php echo site_url(); ?>/index.php" class="btn-dark align-center">Start Order</a>
 </div>
 
 <?php include_once __DIR__ . '/_components/footer.php'; ?>
